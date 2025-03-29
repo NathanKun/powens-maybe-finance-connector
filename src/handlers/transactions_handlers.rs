eae55f20b580b03a1349db559470ef31eb320096
NathanKun <nathanhejunyang@gmail.com>
2025-03-29 23:35:58 +0100

    feat(csv): export to file. add last_update param to export only transations updated after that time.

--- src/handlers/transactions_handlers.rs
+++ src/handlers/transactions_handlers.rs
@@ -1,11 +1,26 @@
-use axum::extract::State;
-use chrono::{DateTime, NaiveDateTime, Utc};
-use tracing::info;
-use tracing::error;
 use crate::app_state::AppState;
 use crate::csv::{TransactionCsv, VecToCsv};
 use crate::genai::run_ai_guess_on_all_transactions;
 use crate::powens::POWENS_DATETIME_FORMAT;
+use axum::http::{HeaderName, HeaderValue, Response};
+use axum::response::{IntoResponseParts, ResponseParts};
+use axum::{
+    body::Body,
+    extract::{Query, State},
+    http::header,
+    response::IntoResponse,
+};
+use chrono::{DateTime, NaiveDateTime, Utc};
+use serde::Deserialize;
+use tracing::error;
+use tracing::info;
+
+const PARAM_DATETIME_FORMAT: &str = "%Y-%m-%d_%H-%M-%S";
+
+#[derive(Deserialize)]
+pub struct TransactionsToCsvParams {
+    last_update: Option<String>,
+}
 
 pub async fn fetch_transactions_from_powens_handler(State(app_state): State<AppState>) -> String {
     run_fetch_transactions_from_powens_job(app_state);
@@ -16,7 +31,7 @@ pub async fn fetch_transactions_from_powens_handler(State(app_state): State<AppS
 pub fn run_fetch_transactions_from_powens_job(app_state: AppState) {
     tokio::spawn(async move {
         info!("Starting job to fetch transactions from Powens.");
-        
+
         let mut latest_last_update: Option<DateTime<Utc>> = None;
 
         let existing_transactions = app_state.transaction_db.data();
@@ -67,27 +82,106 @@ pub fn run_fetch_transactions_from_powens_job(app_state: AppState) {
     });
 }
 
-pub async fn transactions_to_csv_handler(State(app_state): State<AppState>) -> String {
-    let account_db = &app_state.account_db;
-    let transactions_csv: Vec<TransactionCsv> = (&app_state.transaction_db)
-        .data()
-        .iter()
-        // skip those coming == true
-        .filter(|it| !it.coming)
-        .map(|it| {
-            let mut transaction_csv: TransactionCsv = it.into();
+pub async fn transactions_to_csv_handler(
+    Query(params): Query<TransactionsToCsvParams>,
+    State(app_state): State<AppState>,
+) -> Response<Body> {
+    // parse param
+    let mut last_update: Option<DateTime<Utc>> = None;
+    if let Some(last_update_str) = &params.last_update {
+        if let Ok(last_update_native) =
+            NaiveDateTime::parse_from_str(last_update_str, PARAM_DATETIME_FORMAT)
+        {
+            let last_update_utc: DateTime<Utc> = last_update_native.and_utc();
+            last_update = Some(last_update_utc)
+        }
+    }
 
-            let account = account_db.find_by_id(it.id_account).unwrap();
-            transaction_csv.set_account(&account);
+    // filter transactions
+    let mut transaction = (&app_state.transaction_db).data();
+    // keep those coming == false
+    transaction.retain(|it| !it.coming);
+    // keep if transaction.last_update > param.last_update
+    if let Some(last_update_param) = last_update {
+        transaction.retain(|it| {
+            let last_update_native =
+                NaiveDateTime::parse_from_str(&it.last_update, POWENS_DATETIME_FORMAT).unwrap();
+            let last_update_utc: DateTime<Utc> = last_update_native.and_utc();
 
-            if let Some(extras) = app_state.transaction_extras_db.find_by_id(it.id) {
-                transaction_csv.set_extras(&extras);
+            last_update_utc > last_update_param
+        });
+    }
+
+    // if empty, end
+    if transaction.is_empty() {
+        let res_str = if let Some(last_update_str) = &params.last_update {
+            format!("No transactions found with last_update > {last_update_str} found")
+        } else {
+            "No transactions found.".to_string()
+        };
+
+        Response::builder()
+            .status(200)
+            .body(Body::from(res_str))
+            .unwrap()
+    }
+    // export csv
+    else {
+        // find the biggest last_update in transactions, use it to create a download file name
+        let mut biggest_last_update: Option<DateTime<Utc>> = None;
+        for transaction in transaction.iter() {
+            let last_update_native =
+                NaiveDateTime::parse_from_str(&transaction.last_update, POWENS_DATETIME_FORMAT)
+                    .unwrap();
+            let last_update_utc: DateTime<Utc> = last_update_native.and_utc();
+            if let Some(biggest_last_update_inner) = biggest_last_update {
+                if last_update_utc > biggest_last_update_inner {
+                    biggest_last_update = Some(last_update_utc);
+                }
+            } else {
+                biggest_last_update = Some(last_update_utc);
             }
+        }
 
-            transaction_csv
-        })
-        .collect();
-    transactions_csv.to_csv()
+        // convert to Transaction to TransactionCsv
+        let account_db = &app_state.account_db;
+        let transactions_csv: Vec<TransactionCsv> = transaction
+            .iter()
+            .map(|it| {
+                let mut transaction_csv: TransactionCsv = it.into();
+
+                let account = account_db.find_by_id(it.id_account).unwrap();
+                transaction_csv.set_account(&account);
+
+                if let Some(extras) = app_state.transaction_extras_db.find_by_id(it.id) {
+                    transaction_csv.set_extras(&extras);
+                }
+
+                transaction_csv
+            })
+            .collect();
+
+        // result csv
+        let result = transactions_csv.to_csv();
+
+        // convert the result into a http body
+        let body = Body::from(result);
+
+        let filename = format!(
+            "transactions {}.csv",
+            biggest_last_update.unwrap().format(PARAM_DATETIME_FORMAT)
+        );
+
+        Response::builder()
+            .status(200) // Set status code as needed
+            .header(header::CONTENT_TYPE, "text/csv")
+            .header(
+                header::CONTENT_DISPOSITION,
+                format!("attachment; filename=\"{}\"", filename),
+            )
+            .body(body)
+            .unwrap()
+    }
 }
 
 pub async fn list_transactions_handler(State(app_state): State<AppState>) -> String {
